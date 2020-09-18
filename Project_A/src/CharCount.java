0b086c6191716c382426424c589f33ce59d2e809
Deniz <49382147+denzz312@users.noreply.github.com>
2020-09-17 22:11:59 -0400

    ToDo: should work from terminal

---
+++ Project_A/src/CharCount.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class CharCount {
+
+    private static int EOF = -1;
+    private static File srcFile = null;
+    private static String srcFilename = "<srcFilename>";
+
+
+}
