14b96f2cb5622419b92cd7f29f82ee41bc3f9464
Deniz <49382147+denzz312@users.noreply.github.com>
2020-09-18 23:11:55 -0400

    aka first version

--- Project_A/src/CharCount.java
+++ Project_A/src/CharCount.java
@@ -1,10 +1,55 @@
-import java.io.File;
+// charcount.c - Count characters in a sourcefile
 
-public class CharCount {
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+class CharCount {
+
 
     private static int EOF = -1;
     private static File srcFile = null;
     private static String srcFilename = "<srcFilename>";
 
+
+    public static void main(String[] args) throws IOException {
+
+
+        if (args.length != 2) {
+            System.out.println("Usage: copy <src> <dst>");
+            return;
+        }
+
+
+        if (args[0] != null) { // Check <src>
+            srcFilename = args[0];
+            System.out.println("copy: srcFilename = '" + srcFilename + "'");
+            srcFile = new File(srcFilename);
+            if (!srcFile.canRead()) {
+                System.out.println("copy: Cannot open srcFile '" + srcFilename + "'");
+                return;
+            }
+        } else {
+            System.out.println("copy: [OK] srcFilename = '" + srcFilename + "'");
+        }
+
+
+        final FileInputStream srcStream = new FileInputStream(srcFile);
+
+
+        // Execute the character count.
+        int c;
+        int nChars = 0;
+
+        while ((c = srcStream.read()) != EOF) {
+            nChars++;
+        }
+
+        srcStream.close();
+
+        System.out.printf("%d characters\n", nChars);
+
+    }
 
 }
